bbbf812b44a152d309869d31b82dde7cc1993676
Frank Luongo <frankluongo92@gmail.com>
2019-03-09 07:25:17 -0500

    03: Creating Pages

---
+++ src/pages/about-me.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+const AboutMe = () => (
+  <Layout>
+    <div>
+      <h1>This is about us</h1>
+      <p>
+        This is me
+      </p>
+    </div>
+  </Layout>
+)
+
+export default AboutMe;
