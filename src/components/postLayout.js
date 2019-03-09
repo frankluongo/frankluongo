9eb330252a5e4572429b2f1fc32162bb3fd9fd3e
Frank Luongo <frankluongo92@gmail.com>
2019-03-09 12:19:06 -0500

    11: Gatsby Node

---
+++ src/components/postLayout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Layout from './layout';
+
+export default class postLayout extends Component {
+  render() {
+    return (
+      <Layout>
+        whaddup dawg
+      </Layout>
+    )
+  }
+}
