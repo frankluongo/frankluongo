e4b5d19c55934e2ec630de2c46405ea8b3b6eedc
Frank Luongo <frankluongo92@gmail.com>
2019-03-09 08:17:56 -0500

    Added my menu to the site

--- src/components/header.js
+++ src/components/header.js
@@ -2,34 +2,32 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import gatsbyLogo from '../images/gatsby-icon.png';
 import Logo from '../icons/logo';
+import Menu from './menu';
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
       background: `white`,
+      borderBottom: `1px solid #eee`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
+        display: `flex`,
+        justifyContent: `stretch`,
+        alignItems: `center`,
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          <Logo color="#00b2e5" />
-        </Link>
-      </h1>
+    <Link to="/" style={{ textDecoration: `none` }} >
+      <Logo color="#00b2e5" />
+    </Link>
+    <Menu />
+
     </div>
   </header>
 )
---
+++ src/components/menu.js
@@ -0,0 +1,29 @@
+import { Link } from "gatsby"
+import React from "react"
+
+const Menu = () => (
+  <ul style={{ display: `flex`, width: `100%`, padding: `0`, margin: `0`, listStyle: `none`, justifyContent: `space-around` }} >
+    <li>
+      <Link to="/">
+        Home
+      </Link>
+    </li>
+    <li>
+      <Link to="/about-me">
+        About Me
+      </Link>
+    </li>
+    <li>
+      <Link to="/blog">
+        Blog
+      </Link>
+    </li>
+    <li>
+      <Link to="/contact">
+        Contact
+      </Link>
+    </li>
+  </ul>
+)
+
+export default Menu
